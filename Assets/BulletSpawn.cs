57bea28718d1479f14ca6fb5d52177b96114dacf
Babitk21 <77804801+Babitk21@users.noreply.github.com>
2021-03-02 12:10:05 +0530

    shooter game

---
+++ Assets/BulletSpawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawn : MonoBehaviour
+{
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bulletReference=Instantiate(bullet);
+            bulletReference.transform.position = transform.position;
+            Rigidbody rb = bulletReference.GetComponent<Rigidbody>();
+            rb.velocity =transform.rotation*Vector3.forward * 100.0f;
+            
+        }
+    }
+}
---
+++ Assets/ScoreKeper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeper : MonoBehaviour
+{
+    int score = 0;
+    private void OnCollisionEnter(Collision collision)
+    {
+        score++;
+        print("score:" + score);
+    }
+}
